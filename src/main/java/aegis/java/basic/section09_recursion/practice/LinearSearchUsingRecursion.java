026f40dc461169559f435bef91f100e74d8296ed
username <username@domain.com>
2022-08-11 20:45:45 +0300

    recursion practice

--- src/main/java/aegis/java/basic/section09_recursion/practice/LinearSearchUsingRecursion.java
+++ src/main/java/aegis/java/basic/section09_recursion/practice/LinearSearchUsingRecursion.java
@@ -1,4 +1,32 @@
 package aegis.java.basic.section09_recursion.practice;
 
 public class LinearSearchUsingRecursion {
+    public static void main(String[] args) {
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // 6
+        int query = 6;
+
+        System.out.println(findIndex(array, query, 0));
+    }
+
+    private static int findIndex(int[] array, int query) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == query) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // recursion 1
+    private static int findIndex(int[] array, int query, int i) {
+        if (i == array.length) {
+            return -1;
+        } else if (array[i] == query) {
+            return i;
+        } else {
+            return findIndex(array, query, i + 1);
+        }
+    }
+
+    // recursion 2
 }
