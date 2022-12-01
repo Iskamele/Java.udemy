589d4d700bbbe6b4647e460a1b921985e799591d
Iskamele <iskamele@gmail.com>
2022-12-01 12:14:54 +0200

    Необходимо реализовать программу BubbleSort.
    Данная программа должна сортировать массив с целыми числами по возрастанию, используя алгоритм пузырьковой сортировки.
    
    Алгоритм пузырьковой сортировки следующий:
    
    Дано: любой массив с целыми числами;
    
    Необходимо отсортировать данный массив по возрастанию. Массив считается отсортированным по возрастанию, когда любой элемент массива больше (или равен для массива с повторяющими элементами) предыдущего элемента.
    
    Для сортировки необходимо пройтись по массиву слева направо и попарно сравнить предыдущий и последующий элементы. Если предыдущий элемент больше последующего, то необходимо поменять их местами и сравнивать дальше.
    
    После первого прохождения по всем элементам массива, попарного сравнения и попарного обмена САМЫЙ большой по значению элемент будет последним элементом в массиве, т.е. находится на своем месте.
    
    Дальше необходимо снова пройтись по массиву и попарно сравнить элементы. После второго прохождения предпоследний элемент будет на своем месте.
    
    Данный проход нужно повторять до тех пор пока все элементы не окажутся на своих местах. (В самом худшем случае, когда исходный массив отсортирован по убыванию и не содержит повторений, необходимо выполнить n количество проходов, где n - это длина исходного массива

--- src/main/java/aegis/java/basic/section10_practicalTasks/practice/BubbleSortStream.java
+++ src/main/java/aegis/java/basic/section10_practicalTasks/practice/BubbleSortStream.java
@@ -1,7 +1,6 @@
 package aegis.java.basic.section10_practicalTasks.practice;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class BubbleSortStream {
     public static void main(String[] args) {
